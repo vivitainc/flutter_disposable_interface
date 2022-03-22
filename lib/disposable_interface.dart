ff9b17a562caf47248c273658fedd4de94bdce6b
eaglesakura <eagle.sakura@gmail.com>
2022-03-22 10:33:49 +0900

    chg: stdlib_plusから抽出

---
+++ lib/disposable_interface.dart
@@ -0,0 +1,5 @@
+library disposable_interface;
+
+export 'src/async_disposable.dart';
+export 'src/disposable.dart';
+export 'src/disposable_extensions.dart';
--- lib/src/async_disposable.dart
+++ lib/src/async_disposable.dart
@@ -1,8 +1,8 @@
 /// 解放処理が必要なオブジェクト.
-abstract class Disposable {
+abstract class AsyncDisposable {
   /// 管理しているリソースを開放する.
   ///
   /// NOTE.
   /// [dispose]を２回以上コールした場合の挙動は不定である.
-  void dispose();
+  Future dispose();
 }
